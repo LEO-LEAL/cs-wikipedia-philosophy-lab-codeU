e8ed26ff764cb23b3953171a647f720b008ac114
LEO-LEAL <leoleal97@gmail.com>
2016-05-29 18:14:40 -0700

    DONE

--- javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
+++ javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
@@ -11,41 +11,71 @@ import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
 
 public class WikiPhilosophy {
-	
+
 	final static WikiFetcher wf = new WikiFetcher();
-	
+	final static String phil_url = "https://en.wikipedia.org/wiki/Philosophy";
 	/**
-	 * Tests a conjecture about Wikipedia and Philosophy.
-	 * 
-	 * https://en.wikipedia.org/wiki/Wikipedia:Getting_to_Philosophy
-	 * 
-	 * 1. Clicking on the first non-parenthesized, non-italicized link
-     * 2. Ignoring external links, links to the current page, or red links
-     * 3. Stopping when reaching "Philosophy", a page with no links or a page
-     *    that does not exist, or when a loop occurs
-	 * 
-	 * @param args
-	 * @throws IOException
-	 */
+	* Tests a conjecture about Wikipedia and Philosophy.
+	*
+	* https://en.wikipedia.org/wiki/Wikipedia:Getting_to_Philosophy
+	*
+	* 1. Clicking on the first non-parenthesized, non-italicized link
+		* 2. Ignoring external links, links to the current page, or red links
+	  * 3. Stopping when reaching "Philosophy", a page with no links or a page
+	  *    that does not exist, or when a loop occurs
+	*
+	* @param args
+	* @throws IOException
+	*/
 	public static void main(String[] args) throws IOException {
-		
-        // some example code to get you started
-
 		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
-		Elements paragraphs = wf.fetchWikipedia(url);
+		List<String> visit = new ArrayList<String>();
+	 	visit.add(url);
+	 	int parenthesisCount = 0;
+	 	depthFirstParse(url, visit, parenthesisCount);
+	  }
 
-		Element firstPara = paragraphs.get(0);
-		
-		Iterable<Node> iter = new WikiNodeIterable(firstPara);
-		for (Node node: iter) {
-			if (node instanceof TextNode) {
-				System.out.print(node);
+	public static void depthFirstParse (String url, List<String> visit, int parenthesisCount) throws IOException{
+		if (visit.contains(url)){
+			return;
+	  }
+		Elements paragraph = wf.fetchWikipedia(url);
+	  for (int i = 0; i < paragraph.size(); i++) {
+			Element currPara = paragraph.get(i);
+			Iterable<Node> iter = new WikiNodeIterable(currPara);
+			for (Node node: iter) {
+				if (node instanceof TextNode) {
+					if (node.toString().contains("(")) {
+						parenthesisCount++;
+					}
+					if (node.toString().contains(")")) {
+						parenthesisCount--;
+					}
+				}
+				if (parenthesisCount == 0) {
+					if (node instanceof Element) {
+						Element curr = (Element) node;
+						if (curr.tagName().equals("a")){
+							String check = node.attr("abs:href");
+							boolean isValid = true;
+							if (url.equals(check)){
+								isValid = false;
+							}
+							Element par = (Element) curr.parentNode();
+							if (par.tagName().equals("i") || par.tagName().equals("em")){
+								isValid = false;
+							}
+							if (isValid) {
+								visit.add(check);
+								depthFirstParse(check, visit, parenthesisCount);
+								return;
+							}
+						}
+					}
+				}
 			}
-        }
-
-        // the following throws an exception so the test fails
-        // until you update the code
-        String msg = "Complete this lab by adding your code and removing this statement.";
-        throw new UnsupportedOperationException(msg);
+		}
+		System.err.println("Nothing Valid Found");
+		return;
 	}
 }
